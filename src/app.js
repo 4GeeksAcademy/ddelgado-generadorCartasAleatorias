f4006ff323dfff5fbf75dc9485f569e9fe39171f
DaysiMassielka <54219496+DaysiMassielka@users.noreply.github.com>
2025-06-25 08:02:28 +0000

    Generador de Cartas V1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-import "bootstrap";
-import "./style.css";
+// window.onload = function() {
+//   //write your code here
+
+let valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+let corazonDiamantes = ["♦", "♥", "♠", "♣"];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function generateCard() {
+
+  let cardIcon = document.querySelectorAll(".icon");
+  let cardValue = document.getElementById("card-valor");
+  
+
+  const getRandom = (arr) => arr[Math.floor(Math.random() * arr.length)];
+  
+
+ cardValue.textContent = getRandom(valores);
+ const randomIcon = getRandom(corazonDiamantes);
+ 
+ cardIcon.forEach(icon => {
+  icon.textContent = randomIcon
+   randomIcon === "♦" || randomIcon === "♥" ? icon.style.color = "red": icon.style.color = "black"
+});
+  
+
 };
+let buttonCard = document.getElementById("nueva-carta");
+buttonCard.addEventListener("click", generateCard);
+
+
+window.onload = () => {
+  generateCard();
+}
